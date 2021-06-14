00e7a8852f88eb941ce3d984ce074d9723a9181e
DanielDlc <dlc.engcomputacao@gmail.com>
2021-06-14 14:19:27 -0300

    polybar iniciada com dois monitores

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,10 +1,13 @@
-ne instâncias de barras em execução
+#!/usr/bin/env bash
+
+# matar as instâncias de barras em execução
 killall -q polybar
 
 # Espere até que os processos em execução sejam terminados
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # execute a Polybar, usando a configuração padrão ~/.config/polybar/config
-polybar example &
+polybar quake_left_monitor &
+polybar quake_right_monitor &
 
-echo "Polybar lançada..."
+echo "Polybar Quad Damage..."
